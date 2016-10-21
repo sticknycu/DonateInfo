34d0c243b3abb0fc3c481ad5003a95385a18a256
NycuRO <nghergu@yahoo.com>
2016-10-21 22:54:29 +0300

    Create EventListener.java

---
+++ src/main/java/nycuro/EventListener.java
@@ -0,0 +1,24 @@
+package nycuro;
+
+import cn.nukkit.event.EventHandler;
+import cn.nukkit.event.EventPriority;
+import cn.nukkit.event.Listener;
+import cn.nukkit.event.server.ServerCommandEvent;
+
+/**
+ * author: MagicDroidX
+ * NukkitExamplePlugin Project
+ */
+public class EventListener implements Listener {
+    MainClass plugin;
+
+    public EventListener(MainClass plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = false) //DON'T FORGET THE ANNOTATION @EventHandler
+    public void onServerCommand(ServerCommandEvent event) {
+        this.plugin.getLogger().info("ServerCommandEvent is called!");
+        //you can do more here!
+    }
+}
