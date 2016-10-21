384a5a32c4e89ffaa7ba25333638260e36fc1cc8
NycuRO <nghergu@yahoo.com>
2016-10-21 23:31:19 +0300

    Update MainClass.java

--- src/main/java/nycuro/MainClass.java
+++ src/main/java/nycuro/MainClass.java
@@ -41,48 +41,56 @@ public class MainClass extends PluginBase {
 			sender.sendMessage(TextFormat.BLUE + "- /kit starter, /list, /suicide, /home, /homes, /sethome, /delhome       ");
 			sender.sendMessage(TextFormat.BLUE + "- /tpa , /tpaccept, /tpdeny, /warps");
 			sender.sendMessage(TextFormat.RED + "-----------------------------------------");
+			return true;
 		case "vip":
 			sender.sendMessage(TextFormat.RED +"-----------------------------------------");
 			sender.sendMessage(TextFormat.GOLD + "         Permisii VIP:       ");
 			sender.sendMessage(TextFormat.BLUE + "- In plus fata de jucator ( Afla cu /jucator ):       ");
 			sender.sendMessage(TextFormat.BLUE + "- /fly , /getpos");
 			sender.sendMessage(TextFormat.RED + "-----------------------------------------");
+			return true;
 		case "vip+":
 			sender.sendMessage(TextFormat.RED + "-----------------------------------------");
 			sender.sendMessage(TextFormat.GOLD + "         Permisii VIP+:       ");
 			sender.sendMessage(TextFormat.BLUE + "- In plus fata de VIP ( Afla cu /vip ):       ");
 			sender.sendMessage(TextFormat.BLUE + "- /kick , /time, /heal");
 			sender.sendMessage(TextFormat.RED + "-----------------------------------------");
+			return true;
 		case "mod":
 			sender.sendMessage(TextFormat.RED + "-----------------------------------------");
 			sender.sendMessage(TextFormat.GOLD + "         Permisii Mod:       ");
 			sender.sendMessage(TextFormat.BLUE + "- In plus fata de VIP+ ( Afla cu /vip+ ):       ");
 			sender.sendMessage(TextFormat.BLUE + "- /say , /god, /mute, /unmute");
 			sender.sendMessage(TextFormat.RED + "-----------------------------------------");
+			return true;
 		case "admin":
 			sender.sendMessage(TextFormat.RED + "-----------------------------------------");
 			sender.sendMessage(TextFormat.GOLD + "         Permisii Admin:       ");
 			sender.sendMessage(TextFormat.BLUE +  "- In plus fata de Mod ( Afla cu /mod ):       ");
 			sender.sendMessage(TextFormat.BLUE + "- /ban , /ban-ip, /gamemode, /pardon, /pardon-ip");
 			sender.sendMessage(TextFormat.RED + "-----------------------------------------");
+			return true;
 		case "commander":
 			sender.sendMessage(TextFormat.RED + "-----------------------------------------");
 			sender.sendMessage(TextFormat.GOLD + "         Permisii Commander:       ");
 			sender.sendMessage(TextFormat.BLUE + "- In plus fata de Admin ( Afla cu /admin ):       ");
 			sender.sendMessage(TextFormat.BLUE + "- /effect , /give, /tpo, /tpohere, /jump, /repair");
 			sender.sendMessage(TextFormat.RED + "-----------------------------------------");
+			return true;
 		case "neon":
 			sender.sendMessage(TextFormat.RED + "-----------------------------------------");
 			sender.sendMessage(TextFormat.GOLD + "         Permisii Neon:       ");
 			sender.sendMessage(TextFormat.BLUE + "- Aproape toate:       ");
 			sender.sendMessage(TextFormat.BLUE + "- NU ARE OP. OP nu se vinde.");
 			sender.sendMessage(TextFormat.RED + "-----------------------------------------");
+			return true;
 		case "yt":
 			sender.sendMessage(TextFormat.RED + "-----------------------------------------");
 			sender.sendMessage(TextFormat.GOLD + "         Permisii YT:       ");
 			sender.sendMessage(TextFormat.BLUE + "- Ca la VIP+ ( Afla la /vip+ ):       ");
 			sender.sendMessage(TextFormat.BLUE + "- Are in plus /kit youtuber.");
 			sender.sendMessage(TextFormat.RED + "-----------------------------------------");
+			return true;
 		case "ajutor":
 			sender.sendMessage(TextFormat.RED + "-----------------------------------------");
 			sender.sendMessage(TextFormat.GOLD + "         Permisii Ajutor:       ");
@@ -90,6 +98,7 @@ public class MainClass extends PluginBase {
 			sender.sendMessage(TextFormat.BLUE + "- /kick, /time, /say, /broadcast, /ban, /ban-ip");
 			sender.sendMessage(TextFormat.BLUE + "- /pardon, /pardon-ip, /tpo, /tpohere, /weather");
 			sender.sendMessage(TextFormat.RED + "-----------------------------------------");
+			return true;
 		default:
 			return false;
 	}
