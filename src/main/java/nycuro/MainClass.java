93900ead89cc8a1f6ee8e64d8f8f9569d68bc651
NycuRO <nghergu@yahoo.com>
2016-10-21 22:25:31 +0300

    Update MainClass.java

--- src/main/java/nycuro/MainClass.java
+++ src/main/java/nycuro/MainClass.java
@@ -36,45 +36,72 @@ public class MainClass extends PluginBase implements Listener {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         switch (command.getName()) {
-            case "example":
-            				case "donate":
-			$sender->sendMessage(TextFormat::RED."-----------------------------------------");
-			$sender->sendMessage(TextFormat::GOLD."         Doneaza pentru o sectiune:       ");
-			$sender->sendMessage(TextFormat::BLUE."- Puteti dona prin: PaySafe, PayPal, Cod de reincarcare ORANGE       ");
-			$sender->sendMessage(TextFormat::BLUE."- Intra pe:");
-			$sender->sendMessage(TextFormat::BLUE."- mcpe.minecraftmarket.com");
-			$sender->sendMessage(TextFormat::RED."-----------------------------------------");
+		case "jucator":
+			sender.sendMessage(TextFormat.RED."-----------------------------------------");
+			sender.sendMessage(TextFormat.GOLD."         Permisii Jucator:       ");
+			sender.sendMessage(TextFormat.BLUE."- /kit starter, /list, /suicide, /home, /homes, /sethome, /delhome       ");
+			sender.sendMessage(TextFormat.BLUE."- /tpa , /tpaccept, /tpdeny, /warps");
+			sender.sendMessage(TextFormat.RED."-----------------------------------------");
 			return true;
-				case "donateg":
-			$sender->sendMessage(TextFormat::RED."-----------------------------------------");
-			$sender->sendMessage(TextFormat::GOLD."         Doneaza global:       ");
-			$sender->sendMessage(TextFormat::BLUE."- Puteti dona prin: PaySafe, PayPal, Cod de reincarcare ORANGE       ");
-			$sender->sendMessage(TextFormat::BLUE."- Intra pe:");
-			$sender->sendMessage(TextFormat::BLUE."- mcpe.minecraftmarket.com");
-			$sender->sendMessage(TextFormat::RED."-----------------------------------------");
+		case "vip":
+			sender.sendMessage(TextFormat.RED."-----------------------------------------");
+			sender.sendMessage(TextFormat.GOLD."         Permisii VIP:       ");
+			sender.sendMessage(TextFormat.BLUE."- In plus fata de jucator ( Afla cu /jucator ):       ");
+			sender.sendMessage(TextFormat.BLUE."- /fly , /getpos");
+			sender.sendMessage(TextFormat.RED."-----------------------------------------");
 			return true;
-				case "info":
-			$sender->sendMessage(TextFormat::RED."-----------------------------------------");
-			$sender->sendMessage(TextFormat::GOLD."         Pentru a dona scrie:       ");
-			$sender->sendMessage(TextFormat::BLUE."- /donate , /donateg");
-			$sender->sendMessage(TextFormat::GOLD."      Poti intra pe sectiuni astfel:    ");
-			$sender->sendMessage(TextFormat::BLUE."- Apesi pe ce ai in inventar");
-			$sender->sendMessage(TextFormat::BLUE."- Apesi pe NPC de la portale");
-			$sender->sendMessage(TextFormat::GOLD."         Serverul este facut de:       ");
-			$sender->sendMessage(TextFormat::BLUE."- NycuR0");
-			$sender->sendMessage(TextFormat::GOLD."       Serverul este online 24/24.");
-			$sender->sendMessage(TextFormat::GOLD."Forumul serverului este,acolo puteti face cerere de helper si builder:");
-			$sender->sendMessage(TextFormat::BLUE."- http://forum.minecraft-romania.ro/viewforum.php?f=1662");
-			$sender->sendMessage(TextFormat::RED."-----------------------------------------");
+		case "vip+":
+			sender.sendMessage(TextFormat.RED."-----------------------------------------");
+			sender.sendMessage(TextFormat.GOLD."         Permisii VIP+:       ");
+			sender.sendMessage(TextFormat.BLUE."- In plus fata de VIP ( Afla cu /vip ):       ");
+			sender.sendMessage(TextFormat.BLUE."- /kick , /time, /heal");
+			sender.sendMessage(TextFormat.RED."-----------------------------------------");
 			return true;
-				case "rules":
-			$sender->sendMessage(TextFormat::RED."-----------------------------------------");
-			$sender->sendMessage(TextFormat::GOLD."         Regulile sunt pe forum:       ");
-			$sender->sendMessage(TextFormat::BLUE."- http://forum.minecraft-romania.ro/viewforum.php?f=1662");
-			$sender->sendMessage(TextFormat::RED."-----------------------------------------");
+		case "mod":
+			sender.sendMessage(TextFormat.RED."-----------------------------------------");
+			sender.sendMessage(TextFormat.GOLD."         Permisii Mod:       ");
+			sender.sendMessage(TextFormat.BLUE."- In plus fata de VIP+ ( Afla cu /vip+ ):       ");
+			sender.sendMessage(TextFormat.BLUE."- /say , /god, /mute, /unmute");
+			sender.sendMessage(TextFormat.RED."-----------------------------------------");
 			return true;
-				default:
-					return false;
+		case "admin":
+			sender.sendMessage(TextFormat.RED."-----------------------------------------");
+			sender.sendMessage(TextFormat.GOLD."         Permisii Admin:       ");
+			sender.sendMessage(TextFormat.BLUE."- In plus fata de Mod ( Afla cu /mod ):       ");
+			sender.sendMessage(TextFormat.BLUE."- /ban , /ban-ip, /gamemode, /pardon, /pardon-ip");
+			sender.sendMessage(TextFormat.RED."-----------------------------------------");
+			return true;
+		case "commander":
+			sender.sendMessage(TextFormat.RED."-----------------------------------------");
+			sender.sendMessage(TextFormat.GOLD."         Permisii Commander:       ");
+			sender.sendMessage(TextFormat.BLUE."- In plus fata de Admin ( Afla cu /admin ):       ");
+			sender.sendMessage(TextFormat.BLUE."- /effect , /give, /tp, /jump, /repair");
+			sender.sendMessage(TextFormat.RED."-----------------------------------------");
+			return true;
+		case "neon":
+			sender.sendMessage(TextFormat.RED."-----------------------------------------");
+			sender.sendMessage(TextFormat.GOLD."         Permisii Neon:       ");
+			sender.sendMessage(TextFormat.BLUE."- Aproape toate. ):       ");
+			sender.sendMessage(TextFormat.BLUE."- NU ARE OP. OP nu se vinde.");
+			sender.sendMessage(TextFormat.RED."-----------------------------------------");
+			return true;
+		case "yt":
+			sender.sendMessage(TextFormat.RED."-----------------------------------------");
+			sender.sendMessage(TextFormat.GOLD."         Permisii YT:       ");
+			sender.sendMessage(TextFormat.BLUE."- Ca la VIP+ ( Afla la /vip+ ):       ");
+			sender.sendMessage(TextFormat.BLUE."- Are in plus /kit youtuber.");
+			sender.sendMessage(TextFormat.RED."-----------------------------------------");
+			return true;
+		case "ajutor":
+			sender.sendMessage(TextFormat.RED."-----------------------------------------");
+			sender.sendMessage(TextFormat.GOLD."         Permisii Ajutor:       ");
+			sender.sendMessage(TextFormat.BLUE."- Ca la VIP+ ( Afla la /vip+ ):       ");
+			sender.sendMessage(TextFormat.BLUE."- /kick, /time, /say, /broadcast, /ban, /ban-ip");
+			sender.sendMessage(TextFormat.BLUE."- /pardon, /pardon-ip, /tp, /weather");
+			sender.sendMessage(TextFormat.RED."-----------------------------------------");
+			return true;
+		default:
+			return false;
         }
         return true;
     }
